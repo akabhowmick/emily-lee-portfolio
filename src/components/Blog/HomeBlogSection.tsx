838d80e51914ae579d8b713342507285c201cad1
Akash <akabhowmick@gmail.com>
2024-11-23 21:35:55 -0500

    update blog data

--- src/components/Blog/HomeBlogSection.tsx
+++ src/components/Blog/HomeBlogSection.tsx
@@ -9,7 +9,7 @@ export const HomeBlogSection = () => {
         <div className="w-full px-4">
           <div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
             <h2 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4">
-              My recent posts
+              View my gallery!
             </h2>
           </div>
         </div>
@@ -30,16 +30,17 @@ export const HomeBlogSection = () => {
       </div>
       {/* Map through the blogs array to generate BlogCards */}
       <div className="flex flex-wrap lg:flex-nowrap gap-4 mx-auto justify-center">
-        {recentBlogs2.map((blog, index) => (
-          <BlogCard
-            key={index}
-            date={blog.date}
-            title={blog.title}
-            description={blog.description}
-            imageUrl={blog.imageUrl}
-            link={blog.link}
-          />
-        ))}
+        {recentBlogs2.length > 0 &&
+          recentBlogs.map((blog, index) => (
+            <BlogCard
+              key={index}
+              date={blog.date}
+              title={blog.title}
+              description={blog.description}
+              imageUrl={blog.imageUrl}
+              link={blog.link}
+            />
+          ))}
       </div>
     </section>
   );
--- src/data/blogData.ts
+++ src/data/blogData.ts
@@ -5,7 +5,7 @@ import { blog4 } from "./fullBlogs/blog4";
 import author_image from "../assets/Emily-image.png";
 
 export const recentBlogs = [blog2, blog1, blog4];
-export const recentBlogs2 = [blog2, blog1, blog4];
+export const recentBlogs2 = [];
 
 export const fullBlogPosts = [blog2, blog1];
 
