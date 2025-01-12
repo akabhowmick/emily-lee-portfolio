481472af7852dff134512ba51f5676700e7aa8fe
Akash <akabhowmick@gmail.com>
2025-01-11 21:00:18 -0500

    update blog 13

--- src/data/fullBlogs/blog13.ts
+++ src/data/fullBlogs/blog13.ts
@@ -4,19 +4,19 @@ import blog13Image from "../../assets/Art_Pieces/Week_13.png";
 export const blog13: ArtPieceInterface = {
   _id: "13",
   title: "Forest",
-  content: ``,
+  content: `I made this forest drawing out of pastel sticks. I made this drawing because my family has always loved nature and going outside. My parents try to take me and my sister outside as much as possible, so we go on a lot of hikes and walks. `,
   summary:
-    "",
+    "I made this forest drawing out of pastel sticks. I made this drawing because my family has always loved nature and going outside. My parents try to take me and my sister outside as much as possible, so we go on a lot of hikes and walks. ",
   description:
-    "",
+    "I made this forest drawing out of pastel sticks. I made this drawing because my family has always loved nature and going outside. My parents try to take me and my sister outside as much as possible, so we go on a lot of hikes and walks. ",
   author: {
     _id: "1",
     username: "Emily",
   },
-  tag: "".split(" "),
-  label: "",
+  tag: "Pastel Sticks.".split(" "),
+  label: "Pastel Sticks.",
   imageUrl: blog13Image,
   link: "/blogs/week_13",
   readTime: "2 min read",
-  date: "2025-01-",
+  date: "2025-01-15",
 };
