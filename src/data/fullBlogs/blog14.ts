28b9760fd6631025fb840dd6eb4c0166c6dbb58e
Akash <akabhowmick@gmail.com>
2025-01-11 20:15:12 -0500

    update blog 14

--- src/data/fullBlogs/blog14.ts
+++ src/data/fullBlogs/blog14.ts
@@ -3,20 +3,20 @@ import blog14Image from "../../assets/Art_Pieces/Week_14.png";
 
 export const blog14: ArtPieceInterface = {
   _id: "14",
-  title: "",
-  content: ``,
+  title: "Screaming Girl",
+  content: `“Screaming Girl” was a collage that was originally made for scholastics, but I didn’t end up adding in it after discussing it with my art teacher. I used a newspaper that talked about wars and activists to show why the girl was screaming. I used broken pieces of glass, a hot glue gun, a newspaper, and a characol stick. `,
   summary:
-    "",
+    "“Screaming Girl” was a collage that was originally made for scholastics, but I didn’t end up adding in it after discussing it with my art teacher. I used a newspaper that talked about wars and activists to show why the girl was screaming. I used broken pieces of glass, a hot glue gun, a newspaper, and a characol stick. ",
   description:
-    "",
+    "“Screaming Girl” was a collage that was originally made for scholastics, but I didn’t end up adding in it after discussing it with my art teacher. I used a newspaper that talked about wars and activists to show why the girl was screaming. I used broken pieces of glass, a hot glue gun, a newspaper, and a characol stick. ",
   author: {
     _id: "1",
     username: "Emily",
   },
-  tag: "".split(" "),
-  label: "",
+  tag: "Collage".split(" "),
+  label: "Collage",
   imageUrl: blog14Image,
   link: "/blogs/week_14",
-  readTime: "2 min read",
-  date: "2025-01-",
+  readTime: "6 min read",
+  date: "2025-01-25",
 };
