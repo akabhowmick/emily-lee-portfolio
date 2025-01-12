6046435e280e1ce35a826f16521a537176d0a682
Akash <akabhowmick@gmail.com>
2025-01-11 20:29:13 -0500

    update blog 15

--- src/data/fullBlogs/blog15.ts
+++ src/data/fullBlogs/blog15.ts
@@ -4,19 +4,19 @@ import blog15Image from "../../assets/Art_Pieces/Week_15.png";
 export const blog15: ArtPieceInterface = {
   _id: "15",
   title: "",
-  content: ``,
+  content: `Finished this colorful piece made entirely with colored pencils, inspired by my love for M&Ms, which is also the inspiration behind my nickname Em&Em’s.`,
   summary:
-    "",
+    "Finished this colorful piece made entirely with colored pencils, inspired by my love for M&Ms, which is also the inspiration behind my nickname Em&Em’s.",
   description:
-    "",
+    "Finished this colorful piece made entirely with colored pencils, inspired by my love for M&Ms, which is also the inspiration behind my nickname Em&Em’s.",
   author: {
     _id: "1",
     username: "Emily",
   },
-  tag: "".split(" "),
-  label: "",
+  tag: "Colored Pencil Art".split(","),
+  label: "Colored Pencil Art",
   imageUrl: blog15Image,
   link: "/blogs/week_15",
   readTime: "2 min read",
-  date: "2025-01-",
+  date: "2025-01-31",
 };
