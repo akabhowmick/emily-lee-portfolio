1a35595f9643717681e3efdf0ed3fe933c91ed5b
Akash <akabhowmick@gmail.com>
2025-02-08 09:54:53 -0500

    add clownhead blogfull

--- src/data/fullBlogs/blog3.ts
+++ src/data/fullBlogs/blog3.ts
@@ -6,7 +6,16 @@ export const blog3: ArtPieceInterface = {
   title: "ClownHead",
   summary:
     "I initially aimed to create a head inspired by Tim Burton's Corpse Bride, but as it progressed, I wasn't satisfied and shifted my focus to crafting a scary clown. I envisioned a round clown nose, a jester hat, and to enhance its oddities, I added round balls as hair. To further achieve an unsettling effect, I intentionally left the skin rough and textured, avoiding any smoothing to amplify the eerie appearance.",
-  content: `I initially aimed to create a head inspired by Tim Burton's Corpse Bride, but as it progressed, I wasn't satisfied and shifted my focus to crafting a scary clown. I envisioned a round clown nose, a jester hat, and to enhance its oddities, I added round balls as hair. To further achieve an unsettling effect, I intentionally left the skin rough and textured, avoiding any smoothing to amplify the eerie appearance.`,
+  content: `The second project I did for the SVA pre-college program was making a plaster head out of someone you know. My friend and I decided to do each other, but we made each other look funny. My friend made me look like Link from the legend of Zelda and I was originally going to make her into a funny clown, but decided to make it look scary. 
+  //break
+  For this project:
+  //break 1. You had to make your head idea out of clay
+  //break 2. Second you would put plastic rectangles into the middle of the head and then put thick layers of plaster onto the clay head,
+  //break 3. Third you would carefully chisel the plaster mode along the cracks and the plaster would open in half to reveal the clay head that was inside,
+  //break 4. Fourth you would take all the clay out of the head mold and then plaster the plaster head mold closed.
+  //break 5. Fifth you would fill the two sides of the plaster head mold 
+  //break 6. In my opinion I had the most fun doing this project because I like working with clay. Clay is very fun to use in my opinion and I was allowed to make weird textures on the clay and I just had a blast.
+`,
   author: {
     _id: "1",
     username: "Emily ",
